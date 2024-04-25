7591d92eaf8bedade24b073dd5ee91f3681e43c6
Juandque <juanm.duqueg1@uqvirtual.edu.co>
2024-04-25 10:28:23 -0500

    Primera version del front

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { InicioComponent } from './componentes/inicio/inicio.component';
+import { LoginComponent } from './componentes/login/login.component';
+import { RegistroComponent } from './componentes/registro/registro.component';
+import { RouterModule } from '@angular/router';
+export const routes: Routes = [
+{ path: '', component: InicioComponent },
+{ path: 'login', component: LoginComponent },
+{ path: 'registro', component: RegistroComponent },
+{ path: "**", pathMatch: "full", redirectTo: "" }
+];
